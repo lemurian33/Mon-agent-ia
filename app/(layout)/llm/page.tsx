24fb6690e8a24211ae25b9dae8c1b1345ec0a5f4
lemurian33 <lemurianagency@gmail.com>
2026-04-20 15:16:47 +0200

    [Feature] add feature llm page component

--- app/(layout)/llm/page.tsx
+++ app/(layout)/llm/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { OllamaModels } from "@/features/solutions/ollama-models";
+import { FeaturesSection} from "@/features/landing/feature-section";
 
 function page() {
   return (
     <>
+      <FeaturesSection/>
       <OllamaModels />
     </>
   );
